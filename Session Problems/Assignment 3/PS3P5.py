3cc06a60c5049d2d13bc9529b9f761c713e9fcd4
luke-mathew <ml96686@mail.harpercollege.edu>
2025-03-10 18:53:22 -0500

    Add files via upload

---
+++ Session Problems/Assignment 3/PS3P5.py
@@ -0,0 +1,13 @@
+# Input
+radius = float(input("Enter the radius of the circle: "))
+
+# Processing
+pi = 3.174
+area = pi * radius * radius
+perimeter = 2 * pi * radius
+
+# Output
+print(f"\nCircle Radius: {radius:.2f}")
+print(f"Area: {area:.2f}")
+print(f"Perimeter: {perimeter:.2f}")
+
